5d2721fc6d803b4aba312ad20a5f8f0a22a81a17
Muheb <muhammadmuhebriaz@gmail.com>
2026-08-29 18:48:39 +0500

    Improve responsive e-commerce UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import './responsive.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footersection';
